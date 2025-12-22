07f900a7472220bee869783912f738db4ed2ffb2
phongdev02 <ngovanphong2k2@gmail.com>
2025-12-22 11:33:21 +0700

    commit all

--- javascript/script.js
+++ javascript/script.js
@@ -12,21 +12,21 @@
       container.querySelectorAll(":scope > div")[i].classList.add("active");
     });
   }
-    // set about me info
-    var aboutMe = [
-      "Ngô Văn Phong",
-      "0376380174",
-      "Phường 16, Gò Vấp , Hồ Chí Minh",
-      "ngovanphong2k2@gmail.com",
-      "Flutter and Dart Developer",
-      "02/05/2002"
-    ];
+  // set about me info
+  var aboutMe = [
+    "Ngô Văn Phong",
+    "0376380174",
+    "Phường 16, Gò Vấp , Hồ Chí Minh",
+    "ngovanphong2k2@gmail.com",
+    "Flutter and Dart Developer",
+    "02/05/2002",
+  ];
 
-    var aboutMeElements = document.querySelector(".about-me header");
-    aboutMeElements.querySelector(".info h1").innerText = aboutMe[0];
-    aboutMeElements.querySelector(".info p").innerText = aboutMe[4];
-    aboutMeElements.querySelector(".number-phone p").innerText = aboutMe[1];
-    aboutMeElements.querySelector(".email p").innerText = aboutMe[3];
-    aboutMeElements.querySelector(".location p").innerText = aboutMe[2]; 
-    aboutMeElements.querySelector(".date p").innerText = aboutMe[5];
+  var aboutMeElements = document.querySelector(".about-me header");
+  aboutMeElements.querySelector(".info h1").innerText = aboutMe[0];
+  aboutMeElements.querySelector(".info p").innerText = aboutMe[4];
+  aboutMeElements.querySelector(".number-phone p").innerText = aboutMe[1];
+  aboutMeElements.querySelector(".email p").innerText = aboutMe[3];
+  aboutMeElements.querySelector(".location p").innerText = aboutMe[2];
+  aboutMeElements.querySelector(".date p").innerText = aboutMe[5];
 })();
