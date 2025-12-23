156a7f5aa5199e85852fead2b04ede95700c6f6a
phongdev02 <ngovanphong2k2@gmail.com>
2025-12-23 14:56:09 +0700

    23/12/2025 create function next page with navbar and button

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,31 @@
 (() => {
   // src/javascript/script.js
   var container = document.querySelector(".container");
-  var navbar = document.querySelectorAll(".nav-list ul li p");
+  var navbar = document.querySelectorAll(".nav-list ul li");
+  var loading = document.querySelector(".loading");
+
   // change container
   for (let i = 0; i < navbar.length; i++) {
-    navbar[i].addEventListener("click", function () {
-      console.log("clicked ");
-      container.querySelectorAll(":scope > div").forEach((el) => {
-        el.classList.remove("active");
-      });
-      container.querySelectorAll(":scope > div")[i].classList.add("active");
-    });
+    navbar[i].addEventListener("click", () => changePage(i));
   }
+  function changePage(i) {
+      console.log("change page to " + i);
+      container.querySelectorAll(":scope > div").forEach((el) => {
+        el.classList.add("opacity-animation");
+      });
+      setTimeout(() => {
+        ALoading();
+      }, 250);
+      setTimeout(() => {
+        container.querySelectorAll(":scope > div").forEach((el) => {
+          el.classList.remove("active");
+          el.classList.remove("opacity-animation");
+        });
+        container.querySelectorAll(":scope > div")[i].classList.add("active");
+        HLoading();
+      }, 500);
+    }
+
   // set about me info
   var aboutMe = [
     "Ngô Văn Phong",
@@ -29,4 +43,46 @@
   aboutMeElements.querySelector(".email p").innerText = aboutMe[3];
   aboutMeElements.querySelector(".location p").innerText = aboutMe[2];
   aboutMeElements.querySelector(".date p").innerText = aboutMe[5];
+
+  // loading
+  function ALoading() {
+    loading.classList.add("active");
+    btnnext.classList.add("hidden");
+    btnprew.classList.add("hidden");
+  }
+  function HLoading() {
+    loading.classList.remove("active");
+    btnnext.classList.remove("hidden");
+    btnprew.classList.remove("hidden");
+  }
+
+  var btnprew = document.querySelector(".button-change-page .button-left");
+  var btnnext = document.querySelector(".button-change-page .button-right");
+
+  btnprew.addEventListener("click", () => {
+    let activeIndex = 0;
+    let pages = container.querySelectorAll(":scope > div");
+    activeIndex = Array.from(pages).findIndex((el) =>
+      el.classList.contains("active")
+    );
+    console.log(activeIndex);
+    if (activeIndex === 0){
+      changePage(pages.length - 1);
+    }else{
+      changePage(activeIndex - 1);
+    };
+  });
+  btnnext.addEventListener("click", () => {
+    let activeIndex = 0;
+    let pages = container.querySelectorAll(":scope > div");
+    activeIndex = Array.from(pages).findIndex((el) =>
+      el.classList.contains("active")
+    );
+    console.log(activeIndex);
+    if (activeIndex === pages.length - 1){
+      changePage(0);
+    }else{
+      changePage(activeIndex + 1);
+    };
+  });
 })();
